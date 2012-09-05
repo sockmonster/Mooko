133d18f8346bb3bf211684d52a688ee68e0f99c0
libra guest <colinant@gmail.com>
2012-09-05 15:01:30 -0400

    template structure for Matrix functions - unimplemented

--- js/Matrix.js
+++ js/Matrix.js
@@ -1,3 +1,75 @@
 function doSomething() {
-    alert("Hello");
+    alert("Matrix");
+}
+
+/* 
+TODO: implement standard Matrix 3D functions + operations
+TODO: implement relevant rotation functions - ASK OLI
+TODO: function to convert AffTrnsfrm to a format compatible w/ OpenGL
+*/
+function set()
+{    
+    
+}
+
+function toZero()
+{
+}
+
+function toOnes()
+{
+}
+
+function toIdentity()
+{
+}
+
+function add()
+{
+    // add 2 matrices
+}
+
+function subtract()
+{
+    // subtract the second matrix from the first
+}
+
+function mult()
+{
+    // mult by first matrix by the second
+}
+
+function vectormult()
+{
+    // mult matrix with a vector
+}
+function scalarmult()
+{
+    // mult matrix with a real-valued scalar  
+}
+
+function div()
+{
+    // divide matrix values by real-valued scalar
+}
+
+function transpose()
+{
+}
+
+function det()
+{
+}
+
+function invert()
+{
+}
+
+function equals()
+{
+    // check if 2 matrices identical
+}
+
+function toString()
+{
 }
