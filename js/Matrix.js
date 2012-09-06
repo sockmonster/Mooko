0d9e2b3b2241da1fcd216aa60e37a9a55a3a241d
libra guest <colinant@gmail.com>
2012-09-06 18:03:27 -0400

    Matrix.js updated w/ some basic functions

--- js/Matrix.js
+++ js/Matrix.js
@@ -1,32 +1,56 @@
-function doSomething() {
-    alert("Matrix");
-}
-
 /* 
 TODO: implement standard Matrix 3D functions + operations
 TODO: implement relevant rotation functions - ASK OLI
 TODO: function to convert AffTrnsfrm to a format compatible w/ OpenGL
 */
-function set()
-{    
+
+/*--- CONSTRUCTOR ---*/ 
+function Matrix(a11, a12, a13, a21, a22, a23, a31, a32, a33)
+{
+    this.m = [a11, a12, a13, 
+              a21, a22, a23, 
+              a31, a32, a33];
     
-}
-
-function toZero()
-{
-}
-
-function toOnes()
-{
-}
-
-function toIdentity()
-{
+    this.set = function(a11, a12, a13, a21, a22, a23, a31, a32, a33)
+    {
+        this.m = [a11,a12,a13,  a21,a22,a23,  a31,a32,a33];
+        return "set(): what shall i return?";
+    }
+    
+    this.toIdentity = function()
+    {
+        this.m = [1,0,0, 0,1,0, 0,0,1];
+        return "toIdentity(): what shall i return?";
+    }
+    
+    this.toZero = function()
+    {
+        this.m = [0,0,0, 0,0,0, 0,0,0];
+        return "toZero(): what shall i return?";
+    }
+    
+    this.toOnes = function()
+    {
+        this.m = [1,1,1, 1,1,1, 1,1,1];
+        return "toOnes(): what shall i return?";
+    }
+    
+    this.toString = function() 
+    {
+        var ms = "";
+        for(var i = 0; i < 3; i++) {
+            for(var j = 0; j < 3; j++) {
+                ms += this.m[i * 3 + j];
+            }
+            ms += '</br>';
+        }
+        return ms;
+    }
 }
 
 function add()
 {
-    // add 2 matrices
+    
 }
 
 function subtract()
@@ -70,6 +94,11 @@ function equals()
     // check if 2 matrices identical
 }
 
-function toString()
+function toString(M)
 {
+    
 }
+
+
+
+
