1881a099efd29877cb6d41e78e64ca7fcad16f40
Oliver Winks <oliverwinks@gmail.com>
2012-09-17 18:59:59 -0400

    Changed Vector.js to be more memory efficient by adding methods to the prototype object instead of the 'this' object.

--- js/Vector.js
+++ js/Vector.js
@@ -1,69 +1,77 @@
 function Vector2(vec) {
-    this.x = typeof vec === 'undefined' ? 0 : vec.x
-    this.y = typeof vec === 'undefined' ? 0 : vec.y
+    /* ******** ******** ******** */
+    /*      PRIVATE SECTION       */
+    /* ******** ******** ******** */
+    // empty!
     
-    this.setTo = function(vec) {
-        this.x = vec.x
-        this.y = vec.y
-    }
+    /* ******** ******** ******** */
+    /*       PUBLIC SECTION       */
+    /* ******** ******** ******** */
+    this.x = typeof vec === 'undefined' ? 0 : vec.x;
+    this.y = typeof vec === 'undefined' ? 0 : vec.y;
     
-    this.add = function(vec) {
+    Vector2.prototype.setTo = function(vec) {
+        this.x = vec.x;
+        this.y = vec.y;
+    };
+        
+    Vector2.prototype.add = function(vec) {
         return new Vector2({
             x: this.x + vec.x, 
             y: this.y + vec.y
-        })
-    }
-    
-    this.sub = function(vec) {
+        });
+    };
+        
+    Vector2.prototype.subtract = function(vec) {
         return new Vector2({
             x: this.x - vec.x,
             y: this.y - vec.y
-        })
-    }
-    
-    this.mul = function(vec) {
+        });
+    };
+        
+    Vector2.prototype.multiply = function(vec) {
         return new Vector2({
             x: this.x*vec.x,
             y: this.y*vec.y
-        })
-    }
-    
-    this.div = function(vec) {
+        });
+    };
+        
+    Vector2.prototype.divide = function(vec) {
         return new Vector2({
             x: this.x/vec.x,
             y: this.y/vec.y
-        })
-    }
-    
-    this.dot = function(vec) {
-        return this.x*vec.x + this.y*vec.y
-    }
-    
-    this.dist = function(vec) {
-        var xDist = vec.x - this.x
-        var yDist = vec.y - this.y
+        });
+    };
         
-        return Math.sqrt(xDist*xDist + yDist*yDist)
-    }
-    
-    this.norm = function() {
-        var length = this.length()
-        return this.div(length, length)
-    }
-    
-    this.length = function() {
-        return Math.sqrt(this.x*this.x + this.y*this.y)
-    }
-    
-    this.lengthSq = function() {
-        return this.x*this.x + this.y*this.y
-    }
-    
-    this.lerp = function() {
+    Vector2.prototype.dotProduct = function(vec) {
+        return this.x*vec.x + this.y*vec.y;
+    };
+        
+    Vector2.prototype.distanceTo = function(vec) {
+        var xDist = vec.x - this.x;
+        var yDist = vec.y - this.y;
+        
+        return Math.sqrt(xDist*xDist + yDist*yDist);
+    };
+        
+    Vector2.prototype.normalised = function() {
+        var length = this.length();
+        return this.div(length, length);
+    };
+        
+    Vector2.prototype.length = function() {
+        return Math.sqrt(this.x*this.x + this.y*this.y);
+    };
+        
+    Vector2.prototype.lengthSquared = function() {
+        return this.x*this.x + this.y*this.y;
+    };
+        
+    Vector2.prototype.lerp = function(delta) {
         // TODO: implement this!
-    }
-    
-    this.toString = function() {
-        return "[" + this.x + ", " + this.y + "]"
-    }
+    };
+        
+    Vector2.prototype.toString = function() {
+        return "[" + this.x + ", " + this.y +"]";
+    };
 }
